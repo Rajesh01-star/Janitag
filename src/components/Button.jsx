237ee4a4de9b21edc43ed0838a219d555acd84a6
Rajesh01-star <rajeshbehera489@gmail.com>
2023-08-14 18:28:26 +0530

    happy ending

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,13 @@
 export default function Button({ text, type }) {
     if(type === 'big'){
          return (
-        <div className="flex md:w-[268px] w-[200px] md:h-[83px] h-[63px] md:text-[22px] font-[700] text-white justify-center items-center rounded-[10px] bg-gradient-to-r from-[#FFAC75] via-[#FE6468] to-[#AD71F2] cursor-pointer">
+        <div className="flex md:w-[268px] w-[200px] md:h-[83px] h-[63px] md:text-[22px] font-[700] text-white justify-center items-center rounded-[10px] bg-gradient-to-r from-[#FFAC75] via-[#FE6468] to-[#AD71F2] cursor-pointer hover:scale-105 transition-all ease-in-out">
             {text}
         </div>
     )
     }else{
         return (
-        <div className="w-[125px] h-[51px] hidden md:flex text-white justify-center items-center rounded-[10px] bg-gradient-to-r from-[#FFAC75] via-[#FE6468] to-[#AD71F2] cursor-pointer">
+        <div className="w-[125px] h-[51px] hidden md:flex text-white justify-center items-center rounded-[10px] bg-gradient-to-r from-[#FFAC75] via-[#FE6468] to-[#AD71F2] cursor-pointer hover:scale-105 transition-all ease-in-out">
             {text}
         </div>
     )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 export default function Card({ emoji, heading, text }) {
     return (
-        <section className="border-[1px] border-black lg:h-[460px] h-[360px] lg:w-[413px] w-[313px] rounded-[20px] md:rounded-[30px] flex justify-center items-center text-[#333232]">
+        <section className="border-[1px] border-black lg:h-[460px] h-[360px] lg:w-[413px] w-[313px] rounded-[20px] md:rounded-[30px] flex justify-center items-center text-[#333232] cursor-pointer">
             <div className="lg:w-[350px] w-[250px] lg:h-[350px] h-[250px] flex flex-col justify-between items-start">
                 <p className="lg:text-[66px] text-[56px]">{emoji}</p>
                 <div>
