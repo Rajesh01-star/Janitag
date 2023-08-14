96231fd17ed5f9dfdc8d3a5aa75f982d445ec7e9
Rajesh01-star <rajeshbehera489@gmail.com>
2023-08-14 19:39:52 +0530

    last minutes

--- src/components/FollowUp.jsx
+++ src/components/FollowUp.jsx
@@ -3,8 +3,8 @@ import Button from "./Button";
 export default function FollowUp() {
     return (
         <section className="flex flex-col justify-center items-center gap-2 md:my-40 my-20">
-            <h1 className="lg:text-[56px] md:text-[54px] text-[32px] font-[700] text-[#333232] leading-normal">Still not convinced?</h1>
-            <p className="w-1/2 text-center md:text-[22px] text-[14px] font-[400] text-[#5C5C5B] leading-normal mb-8">Schedule a call to learn more about how we can change the way you view sourcing your ads...forever.</p>
+            <h1 className="lg:w-[670px] lg:text-[56px] md:text-[54px] text-[32px] font-[700] text-[#333232] leading-normal text-center">Still not convinced?</h1>
+            <p className="lg:w-[650px] w-1/2 text-center md:text-[22px] text-[14px] font-[400] text-[#5C5C5B] leading-normal mb-8">Schedule a call to learn more about how we can change the way you view sourcing your ads...forever.</p>
             <Button text={"schedule a call"} type={"big"} />
         </section>
     )
