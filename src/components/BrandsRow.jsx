801da9894e72caf730ef15aec232d06ecfbdb753
Rajesh01-star <rajeshbehera489@gmail.com>
2023-08-14 18:17:02 +0530

    finished

--- src/components/BrandsRow.jsx
+++ src/components/BrandsRow.jsx
@@ -1,8 +1,8 @@
 import { brandsSupply } from "../util/constants"
 export default function BrandsRow() {
     return (
-        <section className="flex flex-col md:flex-row md:px-20 px-4 items-center justify-evenly">
-            <div className="w-[200px] md:w-[232px] text-[24px] md:text-[32px] text-center md:text-start text-[#CBBFAC] font-[600] md:font-[700] leading-normal">Brands we've supported</div>
+        <section className="flex flex-col md:flex-row lg:px-20 md:px-10 px-4 items-center lg:justify-evenly">
+            <div className="w-[200px] md:w-[240px] text-[24px] lg:text-[32px] text-center md:text-start text-[#CBBFAC] font-[600] md:font-[700] leading-normal">Brands we've supported</div>
             <div className="flex gap-4 mt-4 md:mt-0 md:gap-20">
                 {brandsSupply.map((brand) => {
                     return (
